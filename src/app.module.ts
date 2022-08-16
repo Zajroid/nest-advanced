8c56247dabc84340ed029accd1c3929fe7ed2fa7
Zajroid <zajroid@gmail.com>
2022-08-16 11:37:53 +0300

    settings sequilize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
-import { DatabaseModule } from './database/database.module';
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
+import { AppService } from "./app.service";
+import { AppController } from "./app.controller";
 import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { ConfigModule } from "@nestjs/config";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import path from "path";
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
-    imports: [DatabaseModule]
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, "static"),
+    }),
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRESS_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRESS_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
