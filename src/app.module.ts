2e2c518f80477496b4447df05b59771536e5aaa7
Zajroid <zajroid@gmail.com>
2022-08-19 09:57:38 +0300

    connection to postresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,29 +2,20 @@ import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
-import { ConfigModule } from "@nestjs/config";
-import { ServeStaticModule } from "@nestjs/serve-static";
-import path from "path";
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: `.${process.env.NODE_ENV}.env`,
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: path.resolve(__dirname, "static"),
-    }),
     SequelizeModule.forRoot({
       dialect: "postgres",
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRESS_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRESS_PASSWORD,
-      database: process.env.POSTGRES_DB,
+      host: 'localhost',
+      port: 5432,
+      username: 'zajroid',
+      password: '1251',
+      database: 'nest-course',
       models: [],
-      autoLoadModels: true,
+      autoLoadModels: true
     }),
   ],
 })
